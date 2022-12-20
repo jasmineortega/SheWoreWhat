b02fbb691d1932daee5d03d9a435faa8a5510310
Jasmine Ortega <jasmineortega29@gmail.com>
2022-12-20 14:12:02 -0800

    feat: closet_cat function

--- src/sheworewhat.py
+++ src/sheworewhat.py
@@ -39,3 +39,47 @@ def closet_df(path="../data/ClosetData.csv"):
             closet["2023"].iloc[i] = "No"
 
     return closet
+
+
+def closet_cat(closet_df):
+    """
+    Function to parse closet df into five distinct datasets
+    1) Tops
+    2) Bottoms
+    3) Full Body
+    4) Accessories
+    5) Shoes
+
+    Parameters:
+    -----------
+        closet_df : pandas.DataFrame
+            Dataframe obtained from closet_df function
+
+    Returns:
+    --------
+        top_df : pandas.DataFrame
+            Dataframe containing only top data.
+
+        bottom_df : pandas.DataFrame
+            Dataframe containing only bottom data.
+
+        fb_df : pandas.DataFrame
+            Dataframe containing only full-body data.
+
+        acc_df : pandas.DataFrame
+            Dataframe containing only accessory data.
+
+        shoes_df : pandas.DataFrame
+            Dataframe containing only shoe data.
+    """
+    categories = ["top", "bottom", "fb", "acc", "shoes"]
+
+    for i in categories:
+        if i != "fb" or i != "acc":
+            globals()[f"{i}_df"] = df[df["Category"] == i.title()]
+
+    # hardcode bc running into global var issue with these two (empty df)
+    acc_df = df[df["Category"] == "Accessory"]
+    fb_df = df[df["Category"] == "Full Body"]
+
+    return top_df, bottom_df, acc_df, fb_df, shoes_df
