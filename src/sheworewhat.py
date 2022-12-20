f7ef0d7d1a052c2dd76a0e4af3c4e15d801bfd37
Jasmine Ortega <jasmineortega29@gmail.com>
2022-12-20 13:11:49 -0800

    Feat: closet_df

--- src/sheworewhat.py
+++ src/sheworewhat.py
@@ -1,1 +1,37 @@
-"""Main module."""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def closet_df(path="../data/ClosetData.csv"):
+    """
+    Function to import CSV data and return df with unique identifiers.
+
+    Parameters:
+    -----------
+        path : str
+            Path to CSV file containing closet information.
+
+    Returns:
+    --------
+        closet : pandas.DataFrame
+            Dataframe containing 12 columns: ID, Item, Category, Subcategory,
+            Color, Pattern, Brand, Bought, Price, 2023, Cost, Name
+    """
+    # avoid setting with copy warning
+    pd.options.mode.chained_assignment = None
+
+    closet = pd.read_csv(path)
+
+    # create IDs per item
+    closet = closet.reset_index().rename(columns={"index": "ID"})
+
+    # create item name
+    closet["Name"] = closet["Brand"] + " " + closet["Item"]
+
+    # NaNs in 2023 addition column
+    for i, value in enumerate(closet["2023"]):
+        if value != "Yes":
+            closet["2023"].iloc[i] = "No"
+
+    return closet
