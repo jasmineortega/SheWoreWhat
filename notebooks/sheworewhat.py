2bdad26d0bb22be44eb8fcce31f33b5b45ffc532
Jasmine Ortega <jasmineortega29@gmail.com>
2022-12-21 21:51:28 -0800

    feats: complete_data function

--- notebooks/sheworewhat.py
+++ notebooks/sheworewhat.py
@@ -30,9 +30,17 @@ def closet_df(path="../data/ClosetData.csv"):
     closet["Item"] = closet["Item"].map(str.title)
     closet["Brand"] = closet["Brand"].map(str.title)
     closet["PrimaryC"] = closet["Color"].str[:5]
-    
-    # create item name 
-    closet["Name"] = closet["ID"].apply(str) + " " + closet["Brand"] + " " + closet["Item"] + " - " + closet["PrimaryC"]
+
+    # create item name
+    closet["Name"] = (
+        closet["ID"].apply(str)
+        + " "
+        + closet["Brand"]
+        + " "
+        + closet["Item"]
+        + " - "
+        + closet["PrimaryC"]
+    )
 
     # NaNs in 2023 addition column
     for i, value in enumerate(closet["2023"]):
@@ -52,42 +60,96 @@ def closet_cat(df):
     4) Outerwear
     5) Shoes
     6) Tops
-    
+
     Parameters:
     -----------
         df : pandas.DataFrame
             Dataframe obtained from closet_df function
-            
+
     Returns:
     --------
-        acc_df : pandas.DataFrame 
+        acc_df : pandas.DataFrame
             Dataframe containing only accessory data.
-            
-        bottom_df : pandas.DataFrame 
+
+        bottom_df : pandas.DataFrame
             Dataframe containing only bottom data.
-            
-        fb_df : pandas.DataFrame 
+
+        fb_df : pandas.DataFrame
             Dataframe containing only full-body data.
-            
-        out_df : pandas.DataFrame 
+
+        out_df : pandas.DataFrame
             Dataframe containing only outerwear data.
-            
-        shoes_df : pandas.DataFrame 
+
+        shoes_df : pandas.DataFrame
             Dataframe containing only shoe data.
-            
-        top_df : pandas.DataFrame 
+
+        top_df : pandas.DataFrame
             Dataframe containing only top data.
-            
+
     """
     categories = ["top", "bottom", "fb", "outerwear", "acc", "shoes"]
 
     for i in categories:
         if i != "fb" or i != "acc":
-            globals()[f'{i}_df'] = df[df["Category"] == i.title()]
+            globals()[f"{i}_df"] = df[df["Category"] == i.title()]
 
     # hardcode bc running into global var issue with these two (empty df)
     acc_df = df[df["Category"] == "Accessory"]
     fb_df = df[df["Category"] == "Full Body"]
     out_df = df[df["Category"] == "Outerwear"]
-    
+
     return acc_df, bottom_df, fb_df, out_df, shoes_df, top_df
+
+
+def complete_df(closet, path="../data/2023TestData.csv"):
+    """
+    Function to merge raw closet data and collected 2023 data.
+
+    Parameters
+    ----------
+        closet : pandas.DataFrame
+            Dataframe containing complete closet log.
+        path : string
+            String containing path of CSV of collected data.
+
+    Returns
+    -------
+        complete_df : pandas.DataFrame
+            Dataframe containing "ID", "Name", "count", "Item",
+            "Category", "Sub-Category", "Color", "Pattern", "Brand", "Cost", "2023"
+    """
+
+    form = pd.read_csv(path).drop("Timestamp", axis=1).melt("Date").dropna()
+
+    # extract ID number from value
+    form["ID"] = form.value.str.extract("(\d+)").astype(int)
+
+    form_counts = (
+        form.groupby(["value", "ID"])
+        .count()
+        .reset_index()
+        .rename(columns={"Date": "count"})
+        .drop(["variable"], axis=1)
+    )
+
+    # left join closet + df
+    complete_df = pd.merge(closet, form_counts, how="left", on="ID")
+    complete_df["Name"] = complete_df["Brand"] + " " + complete_df["Item"]
+    complete_df = complete_df[
+        [
+            "ID",
+            "Name",
+            "count",
+            "Item",
+            "Category",
+            "Sub-Category",
+            "Color",
+            "Pattern",
+            "Brand",
+            "Cost",
+            "2023",
+        ]
+    ]
+    complete_df = complete_df.fillna(0)
+
+    return complete_df
