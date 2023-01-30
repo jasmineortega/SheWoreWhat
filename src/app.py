ceaa4af66bd19440dd08423429730015fa4c499a
Jasmine Ortega <jasmineortega29@gmail.com>
2023-01-29 17:36:51 -0800

    style: color

--- src/app.py
+++ src/app.py
@@ -152,7 +152,7 @@ def plot_mostworn(
     item_name="Adidas Tennis Shoe",
     i=10,
     title="Ten Most Worn Pieces in 2023",
-    highlight="#d8f2ec",
+    highlight="#6fccf2",
 ):
     most_worn = worn_df.nlargest(i, columns="Count")
     closet_comp = (
@@ -464,7 +464,7 @@ def plot_heatmap(top_10, df, z=0):
             alt.Y("Day", sort=weekdays, title=""),
             alt.Color(
                 "Bool",
-                scale=alt.Scale(domain=[0, 1], range=["#e0ddd5", "#d8f2ec"]),
+                scale=alt.Scale(domain=[0, 1], range=["#e0ddd5", "#6fccf2"]),
                 legend=None,
             ),
             alt.Tooltip(["Date", "Day"]),
