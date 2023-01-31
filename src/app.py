d64525c6771a1262374ef6dfd2e8919e793b1d8f
Jasmine Ortega <jasmineortega29@gmail.com>
2023-01-30 16:00:02 -0800

    style: cpw diamond

--- src/app.py
+++ src/app.py
@@ -524,7 +524,7 @@ def plot_cpw():
 
     plot = (
         alt.Chart(complete_df, title="2023 Cost Per Wear (CPW)")
-        .mark_point(opacity=0.70, size=200, shape="diamond")
+        .mark_point(opacity=0.40, size=200)
         .encode(
             alt.X("Price", scale=alt.Scale(domain=(0, 185))),
             alt.Y("Count", scale=alt.Scale(domain=(0, 25)), title="Times Worn"),
