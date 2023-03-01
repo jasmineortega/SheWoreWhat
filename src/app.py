9472e339e8d2ea39a03fa21ddf99afeb5c7bced1
Jasmine Ortega <jasmineortega29@gmail.com>
2023-02-28 19:57:49 -0800

    text: add info

--- src/app.py
+++ src/app.py
@@ -804,6 +804,12 @@ app.layout = dbc.Container(
                                                         f"In 2023, I've added {len(df_2023)} new items to my closet and "
                                                         f"spent a grand total of ${annual_spent:.2f}. "
                                                     ),
+                                                    html.Br(),
+                                                    html.P(
+                                                        f"Of these new items, I'm happy to report that {new_percent_thrifted:.2f}% were pre-loved, "
+                                                        "In an effort to make my closet more sustainable, it's my goal for the majority of my closet to be secondhand! "
+                                                        f"Currently {all_percent_thrifted:.2f}% of my closet is secondhand."
+                                                    ),
                                                 ]
                                             ),
                                             dbc.Row(
@@ -827,15 +833,6 @@ app.layout = dbc.Container(
                                                             ),
                                                         ],
                                                         width={"size": 5},
-                                                    ),
-                                                    dbc.Col(
-                                                        [
-                                                            html.P(
-                                                                f"Of these new items, I'm happy to report that {new_percent_thrifted:.2f}% were pre-loved, "
-                                                                "In an effort to make my closet more sustainable, it's my goal for the majority of my closet to be secondhand! "
-                                                                f"Currently {all_percent_thrifted:.2f}% of my closet is secondhand."
-                                                            ),
-                                                        ]
                                                     ),
                                                 ]
                                             ),
